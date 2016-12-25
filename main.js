48c6a3fb80e30d3e5de85775a7052eafc1902691
nayunhwan <kbk9288@gmail.com>
2016-12-26 03:21:09 +0900

    Git Parsing

--- main.js
+++ main.js
@@ -10,20 +10,53 @@ $(document).ready(function(){
         var m = moment();
 
         console.log(m);
-        data.forEach(function(event){
+        for(var i = 0; i < 20; i++){
+
+          var event = data[i];
           console.log(event);
-          
+
           var actor = event.actor;
           var repo = event.repo;
           var type = event.type;
           var date = event.created_at;
+          var payload = event.payload;
+          console.log(repo.name);
 
           // 오늘로부터 며칠이 지났는지 알 수 있는 함수
           // Moment.js를 이용한다.
           console.log( moment().diff(moment(date), "days") + "day");
 
-        })
-      }
-  })
+          var msg = $('#github_msg').text('');
 
-})
+
+          switch (type) {
+            case "CreateEvent":
+              msg.append($('<i/>').attr('class', 'xi-branch xi-x'))
+              msg.append($('<a/>').attr('href', actor.url).text(actor.login));
+
+              if(payload.ref_type == "branch"){
+                msg.append(' created branch ' + payload.ref + ' at ');
+                msg.append($('<a/>').attr('href', repo.url).text(repo.name))
+              }
+              else if(payload.ref_type == "repository"){
+                msg.append(' created repository ' );
+                msg.append($('<a/>').attr('href', repo.url).text(repo.name))
+              }
+
+
+              break;
+            case "PushEvent":
+              msg.append($('<i/>').attr('class', 'xi-log xi-x'))
+              msg.append($('<a/>').attr('href', actor.url).text(actor.login));
+              msg.append(' pushed to ' + payload.ref + ' at ');
+              msg.append($('<a/>').attr('href', repo.url).text(repo.name))
+
+
+              break;
+            default:
+
+          }
+      }
+    }
+  })
+});
